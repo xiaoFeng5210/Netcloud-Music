13207d97e2f82a5eca17adc1d1feab8fe5956b0c
Qingfeng Zhang <qingfeng.zhang@chongplus.cn>
2020-03-24 18:48:59 +0800

    样式修改

--- cloud-music/src/application/Home/index.js
+++ cloud-music/src/application/Home/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { renderRoutes } from "react-router-config";
 import { HashRouter, Switch, BrowserRouter, Route } from "react-router-dom";
+import { Top } from "./style";
 
 function Home(props) {
   const { route } = props;
 
   return (
     <div>
-      <div>Home</div>
+      <Top>
+        <span className="iconfont menu">&#xe65c;</span>
+        <span className="title">小风音乐</span>
+        <span className="iconfont search">&#xe62b;</span>
+      </Top>
       {/* <BrowserRouter>
         <Switch>{renderRoutes(route.routes)}</Switch>
       </BrowserRouter> */}
