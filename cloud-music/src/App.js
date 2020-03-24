f70f7a6a17cb494ccf1d612e5db4f0cf4f524b36
Qingfeng Zhang <qingfeng.zhang@chongplus.cn>
2020-03-24 18:22:25 +0800

    增加路由和子路由

--- cloud-music/src/App.js
+++ cloud-music/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { IconStyle } from "./assets/iconfont/iconfont";
 import { GlobalStyle } from "./style";
+import { renderRoutes } from "react-router-config"; //renderRoutes 读取路由配置转化为 Route 标签
+import routes from "./routes/index.js";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle></GlobalStyle>
       <IconStyle></IconStyle>
+      {renderRoutes(routes)}
     </div>
   );
 }
