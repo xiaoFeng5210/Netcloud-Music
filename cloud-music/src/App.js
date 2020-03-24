cd6e2ff62814b9c917a1ed7972dc0e5177e65977
Qingfeng Zhang <qingfeng.zhang@chongplus.cn>
2020-03-24 17:31:13 +0800

    项目初始搭建

---
+++ cloud-music/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { IconStyle } from "./assets/iconfont/iconfont";
+import { GlobalStyle } from "./style";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle></GlobalStyle>
+      <IconStyle></IconStyle>
+    </div>
+  );
+}
+
+export default App;
